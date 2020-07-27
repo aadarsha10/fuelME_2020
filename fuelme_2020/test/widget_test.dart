798d4abf0167f834ccd5b1934d2d2ef78fc679c8
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2020-07-27 13:31:02 +0545

    Merging commit  features-> master

--- fuelme_2020/test/widget_test.dart
+++ fuelme_2020/test/widget_test.dart
@@ -5,8 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -31,5 +29,5 @@ void main() {
 
     // logging output.
     print("Testing Widgets");
-  }); 
+  });
 }
